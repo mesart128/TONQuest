4d3505a40acf5151768f2d1859c703673b788a6a
Pulzze92 <76836870+Pulzze92@users.noreply.github.com>
2024-11-20 11:03:07 +0100

    Ðsync 2

--- client/src/pages/GreetingPage.tsx
+++ client/src/pages/GreetingPage.tsx
@@ -1,37 +1,30 @@
 import React from 'react';
 import logo from '../assets/greeting-logo.png';
+import GradientButton from "../components/buttons/GradientButton.tsx";
+import { useNavigate } from 'react-router-dom';
 
 const GreetingPage = () => {
+    const navigate = useNavigate();
+
     const handleContinue = () => {
-        // Handle continue button click
-        console.log('Continue clicked');
+        navigate('/quest');
     };
 
     return (
-        <div className="min-h-screen relative bg-gradient-to-b from-black via-emerald-600 to-black flex flex-col items-center px-6">
-            <div className="mt-12 text-2xl font-bold text-white">
+        <div className="min-h-screen relative bg-gradient-to-b from-black via-[#00a1ff] to-black flex flex-col items-center px-6">
+            <div className="mt-12 text-2xl font-bold text-[#0096FF]">
                 TONQuest
             </div>
 
-            <div className="flex-1 flex flex-col items-center justify-center text-center max-w-md -mt-20">
+            <div className="flex-1 flex flex-col items-center justify-center text-center max-w-md -mt-20 min-h-[500px]">
                 <img src={logo}/>
                 <h1 className="text-3xl font-bold text-white mb-4">
-                    Hi, Artur <span className="inline-block ml-1">👋</span>
+                    Hi, Artur
                 </h1>
-                <p className="text-white/90 text-center leading-relaxed mb-12">
-                    Welcome to TONQuest! Are you ready to dive into the amazing world of cryptocurrencies? It's easier than ever with our app! Complete tasks, earn points and become a real expert in the field of blockchain.
+                <p className="text-white/80 text-center leading-6 mb-12 w-96">
+                    Welcome to TONQuest Are you ready to dive into the amazing world of cryptocurrencies? It's easier than ever with our app! Complete tasks, earn points and become a real expert in the field of blockchain.
                 </p>
-                <button
-                    onClick={handleContinue}
-                    className="w-full py-4 px-8 bg-blue-500/30 hover:bg-blue-500/40
-                     text-white font-semibold rounded-2xl
-                     backdrop-blur-sm transition-all duration-300
-                     border border-white/10
-                     active:transform active:scale-[0.98]
-                     shadow-lg"
-                >
-                    Continue
-                </button>
+               <GradientButton children="Continue" onClick={handleContinue}/>
             </div>
 
             <div className="h-16" />
