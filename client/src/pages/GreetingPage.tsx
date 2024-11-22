3c68c113cf6b415e93d8329b8f8fe14cf6e782a5
dim-anis <anis.dim@gmail.com>
2024-11-22 22:09:15 +0700

    fix: move button to the bottom of the page

--- client/src/pages/GreetingPage.tsx
+++ client/src/pages/GreetingPage.tsx
@@ -11,21 +11,24 @@ const GreetingPage = () => {
   };
 
   return (
-    <div className="min-h-screen relative bg-gradient-to-b from-black via-[#00a1ff] to-black flex flex-col items-center px-6">
-      <div className="mt-12 text-2xl font-bold text-[#0096FF]">TONQuest</div>
+    <div className="min-h-screen relative bg-gradient-to-b from-black via-[#00a1ff] to-black flex flex-col items-center px-16 py-8">
+      <div className="text-2xl font-extrabold text-[#0096FF]">TONQuest</div>
 
-      <div className="flex-1 flex flex-col items-center justify-center text-center max-w-md -mt-20 min-h-[500px]">
-        <img src={logo} />
-        <h1 className="text-3xl font-bold text-white mb-4">Hi, Artur</h1>
-        <p className="text-white/80 text-center leading-6 mb-12 w-96">
-          Welcome to TONQuest Are you ready to dive into the amazing world of
-          cryptocurrencies? It's easier than ever with our app! Complete tasks,
-          earn points and become a real expert in the field of blockchain.
-        </p>
-        <GradientButton children="Continue" onClick={handleContinue} />
+      <div className="flex-1 flex flex-col justify-center max-w-md min-h-[500px]">
+        <div className="flex flex-col items-center justify-center gap-4">
+          <img src={logo} />
+          <div className="gap-4 text-center">
+            <h1 className="text-3xl font-bold text-white mb-4">Hi, Artur</h1>
+            <p className="text-white text-center leading-5 mb-12 w-96">
+              Welcome to TONQuest Are you ready to dive into the amazing world
+              of cryptocurrencies? It's easier than ever with our app! Complete
+              tasks, earn points and become a real expert in the field of
+              blockchain.
+            </p>
+          </div>
+        </div>
       </div>
-
-      <div className="h-16" />
+      <GradientButton children="Continue" onClick={handleContinue} />
     </div>
   );
 };
