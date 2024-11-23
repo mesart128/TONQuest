177a35ebfc1ca3040dce5117056b60c7545ef516
dim-anis <anis.dim@gmail.com>
2024-11-23 11:28:58 +0700

    fix: add horizontal padding, reduce the gap between the icon and the text

--- client/src/components/ConnectWalletBox.tsx
+++ client/src/components/ConnectWalletBox.tsx
@@ -9,10 +9,10 @@ const ConnectWalletBox = () => {
     <div className="bg-gradient-to-r from-[#9333EA] via-[#3B82F6] to-[#16A34A] rounded-xl my-0.5 px-0.5 flex items-center justify-between">
       <button
         onClick={open}
-        className="p-1 rounded-xl hover:bg-primary/20 transition-all flex items-center flex-row gap-4"
+        className="py-1 px-2 rounded-xl hover:bg-primary/20 transition-all flex items-center flex-row"
       >
-        <Wallet />
-        <h3 className="text-2xl text-white text-sm">Connect wallet</h3>
+        <Wallet className="mr-2 stroke-white" />
+        <h3 className="text-white text-sm font-semibold">Connect wallet</h3>
       </button>
     </div>
   );
